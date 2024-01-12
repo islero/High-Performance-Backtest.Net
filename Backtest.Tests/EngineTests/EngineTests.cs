9185f40d8440e265e36b28761226d012718330ba
Yurii Dumanskyi <yura.dumanskyy@gmail.com>
2024-01-12 21:14:40 +0100

    Added TestWarmupCandlesResultCount to make sure Engive returns correct candles count number

--- Backtest.Tests/EngineTests/EngineTests.cs
+++ Backtest.Tests/EngineTests/EngineTests.cs
@@ -99,11 +99,51 @@ namespace Backtest.Tests.EngineTests
             await engine.RunAsync(data, tokenSource.Token);
         }
 
+        [Fact]
+        public async Task TestWarmupCandlesResultCount()
+        {
+            var tokenSource = new CancellationTokenSource();
+
+            int warmupCandlesCount = 3;
+            var trade = new TestTrade();
+
+            bool allWarmupCandlesResultsAreCorrect = true;
+
+            var strategy = new TestStrategy();
+            strategy.ExecuteStrategyDelegate = (symbols) =>
+            {
+                // --- Checking candles order
+                if (symbols.Any())
+                {
+                    foreach (var symbol in symbols)
+                    {
+                        foreach (var timeframe in symbol.Timeframes)
+                        {
+                            int candlesCount = timeframe.Candlesticks.Count();
+
+                            allWarmupCandlesResultsAreCorrect = allWarmupCandlesResultsAreCorrect && candlesCount == warmupCandlesCount + 1;
+                        }
+                    }
+                }
+
+                tokenSource.Cancel();
+            };
+
+            var engine = new EngineV1(warmupCandlesCount, trade, strategy);
+
+            // --- Generate Dummy SymbolData splitter
+            var data = GenerateCandles(new DateTime(2023, 1, 1), 500, 1, warmupCandlesCount);
+
+            await engine.RunAsync(data, tokenSource.Token);
+
+            Assert.True(allWarmupCandlesResultsAreCorrect);
+        }
+
         /// <summary>
         /// Generates Dummy Data Splitter Result
         /// </summary>
         /// <returns></returns>
-        public IEnumerable<IEnumerable<ISymbolData>> GenerateCandles(DateTime startingDate, int totalCandlesCount, int daysPerSplit, int warmupCandlesCount)
+        private IEnumerable<IEnumerable<ISymbolData>> GenerateCandles(DateTime startingDate, int totalCandlesCount, int daysPerSplit, int warmupCandlesCount)
         {
             ISymbolDataSplitter symbolDataSplitter = new SymbolDataSplitterV1(daysPerSplit, warmupCandlesCount, startingDate, true);
 
